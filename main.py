076e05f30409d3f530dd5a5a14ea0efbffaf4772
amdoi7 <3285109133@qq.com>
2024-09-20 11:54:14 +0800

    update:分批次预约

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from utils.hdulib import (
 )
 
 
-def create_task_from_config(config_dict: dict) -> Task:
+def create_task_from_config(config_dict: dict) -> List[Task]:
     user_name = config_dict.get("user_name")
     password = config_dict.get("password")
     floor_id = str(config_dict.get("floor_id"))
@@ -21,21 +21,31 @@ def create_task_from_config(config_dict: dict) -> Task:
     max_trials = int(config_dict.get("max_trials"))
     interval = int(config_dict.get("interval"))
 
-    return Task(
-        user_name=user_name,
-        password=password,
-        floor_id=floor_id,
-        seat_number=seat_number,
-        begin_time=(
-            dt.datetime.now()
-            + dt.timedelta(days=(1 if floor_id in ["1547", "1548"] else 2))
-        )
-        .replace(hour=begin_time, minute=0, second=0, microsecond=0)
-        .timestamp(),
-        duration=duration,
-        max_trials=max_trials,
-        interval=interval,
+    tasks = []
+    start_time = dt.datetime.now() + dt.timedelta(
+        1 if floor_id in ["1547", "1548"] else 2
     )
+
+    while duration > 0:
+        task_duration = min(duration, 4 if floor_id in ["1547", "1548"] else duration)
+        tasks.append(
+            Task(
+                user_name=user_name,
+                password=password,
+                floor_id=floor_id,
+                seat_number=seat_number,
+                begin_time=start_time.replace(
+                    hour=begin_time, minute=0, second=0, microsecond=0
+                ).timestamp(),
+                duration=task_duration,
+                max_trials=max_trials,
+                interval=interval,
+            )
+        )
+        duration -= task_duration
+        begin_time += task_duration
+    print(len(tasks))
+    return tasks
 
 
 def parse_config(config: str) -> List[Task]:
@@ -44,8 +54,7 @@ def parse_config(config: str) -> List[Task]:
 
     for user_config in user_configs:
         config_dict = dict(re.findall(r"(\w+)\s*=\s*(\S+)", user_config.strip()))
-        task = create_task_from_config(config_dict)
-        tasks.append(task)
+        tasks.extend(create_task_from_config(config_dict))
 
     return tasks
 
@@ -53,6 +62,7 @@ def parse_config(config: str) -> List[Task]:
 async def run(task: Task):
     user = HDULIB(task)
     seat_id = get_seat_by_room_and_floor(HDULIB.rooms, task.floor_id, task.seat_number)
+
     if seat_id == 0:
         print(
             f"user_name: {task.user_name} floor_id: {task.floor_id}, seat_number: {task.seat_number} is not found"
@@ -65,10 +75,12 @@ async def run(task: Task):
     if task.begin_time < dt.datetime.now().timestamp():
         print(f"user_name: {task.user_name} begin_time 已调整到下一天")
         task.begin_time += int(dt.timedelta(days=1).total_seconds())
+
+    start_day_offset = 1 if task.floor_id in ["1547", "1548"] else 2
     if (
-        dt.datetime.fromtimestamp(task.begin_time).day
-        - (1 if task.floor_id in ["1547", "1548"] else 2)
-    ) == dt.datetime.now().day:
+        dt.datetime.fromtimestamp(task.begin_time).day - start_day_offset
+        == dt.datetime.now().day
+    ):
         if dt.datetime.now().hour < 20:
             print("等待到20点开始执行")
             await asyncio.sleep(
@@ -83,7 +95,6 @@ async def run(task: Task):
         if message == "ok":
             print(f"task_id: {task.user_name} Seat reservation successful.")
             break
-
         await asyncio.sleep(task.interval)
 
 
